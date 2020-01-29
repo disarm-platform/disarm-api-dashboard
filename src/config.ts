6b9262fa047787cc56828421f33d74dec056eb8f
superuser-chi <mavusogiftntokozo@gmail.com>
2020-01-29 14:13:28 +0200

    add default config

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,17 @@
 export default {
   api_url: 'https://europe-west1-disarm-platform.cloudfunctions.net/disarm-api-dashboard-api',
+  default_deploy_params: {
+    labels: {
+      // must be the string of true
+      'com.openfaas.scale.zero': 'true',
+    },
+    secrets: [],
+    envVars: {
+      write_debug: 'true',
+      exec_timeout: '600',
+      write_timeout: '700',
+      read_timeout: '700',
+      combine_output: 'alse',
+    },
+  },
 };
