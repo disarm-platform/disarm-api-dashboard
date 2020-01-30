9e2569b95dba7becaba3c9485cba3d7312d93625
superuser-chi <mavusogiftntokozo@gmail.com>
2020-01-30 12:21:26 +0200

    add undeploy function

--- src/undeploy.ts
+++ src/undeploy.ts
@@ -1,3 +1,14 @@
-export function undeploy(function_name: string) {
-  return;
+import CONFIG from '@/config';
+
+export async function undeploy(function_name: string): Promise<string> {
+  const undeploy_url = `${CONFIG.api_url}/undeploy/${function_name}`;
+  try {
+    const data = await fetch(undeploy_url);
+    const message = await data.text();
+    console.log(message);
+    return message;
+  } catch (error) {
+    console.error(error);
+    throw (error) ;
+  }
 }
