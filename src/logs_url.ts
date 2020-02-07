af5c5a72639d7a9d91c6122458b63c314576fe10
superuser-chi <mavusogiftntokozo@gmail.com>
2020-02-07 15:13:53 +0200

    change logd url

--- src/logs_url.ts
+++ src/logs_url.ts
@@ -11,17 +11,7 @@ export function logs_url(function_name: string): string {
     interval: 'NO_LIMIT',
     resource: 'gce_instance/instance_id/9017316993004217383',
     dateRangeUnbound: 'forwardInTime',
-    advancedFilter: `resource.type="gce_instance"
-%0Aresource.labels.instance_id="8492281773921412319"
-%0AlogName="projects/disarm-platform/logs/gcplogs-docker-driver"
-%0ANOT jsonPayload.container.imageName:openfaas
-%0ANOT jsonPayload.container.imageName:traefik
-%0ANOT jsonPayload.container.imageName:portainer
-%0ANOT jsonPayload.container.imageName:squid
-%0ANOT jsonPayload.container.imageName:prometheus
-%0ANOT jsonPayload.container.imageName:alertmanager
-%0AjsonPayload.container.imageName:${function_name}
-%0A`,
+    advancedFilter: `jsonPayload.container.imageName:${function_name}`,
   };
   const joined_params = Object.entries(params).map(([key, value]) => {
     return `${key}=${value}&`;
