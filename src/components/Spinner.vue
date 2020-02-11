ccfb6dd1fda3149a7ee20101b7538750e5d345c4
superuser-chi <mavusogiftntokozo@gmail.com>
2020-02-11 10:49:06 +0200

    add loading spinner

---
+++ src/components/Spinner.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="sbl-circ"></div>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  name: 'spinner',
+});
+</script>
+<style scoped>
+.sbl-circ {
+  height: 1.25em;
+  width: 1.25em;
+  color: #245AC4;
+  position: relative;
+  display: inline-block;
+  border: 0.2em solid;
+  border-radius: 50%;
+  border-top-color: transparent;
+  animation: rotate 1s linear infinite; }
+
+@keyframes rotate {
+  0% {
+    transform: rotate(0); }
+  100% {
+    transform: rotate(360deg); } }
+</style>
