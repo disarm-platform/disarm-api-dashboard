3466d1f282e326b72fc985e760f8a00a750b6091
Jonathan Smith <jonathan@peoplesized.com>
2020-02-12 07:04:29 +0200

    think it's working...

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,9 +3,12 @@ import Deploy from '@/views/Deploy.vue';
 import Undeploy from '@/views/Undeploy.vue';
 import Test from '@/views/Test.vue';
 import Vue from 'vue';
-import VueRouter from 'vue-router';
+import VueRouter, { Route, RawLocation } from 'vue-router';
 
 Vue.use(VueRouter);
+
+// Next is not an exported type in VueRouter
+type Next = (to?: RawLocation | false | ((vm: Vue) => any) | void) => void;
 
 const routes = [
   {
@@ -18,18 +21,42 @@ const routes = [
     name: 'deploy',
     component: Deploy,
     props: true,
+    beforeEnter: (to: Route, from: Route, next: Next) => {
+      console.log(to, from);
+      if (to.params.hasOwnProperty('row')) {
+        next();
+      } else {
+        next('/');
+      }
+    },
   },
   {
     path: '/undeploy',
     name: 'undeploy',
     component: Undeploy,
     props: true,
+    beforeEnter: (to: Route, from: Route, next: Next) => {
+      console.log(to, from);
+      if (to.params.hasOwnProperty('row')) {
+        next();
+      } else {
+        next('/');
+      }
+    },
   },
   {
     path: '/test',
     name: 'test',
     props: true,
     component: Test,
+    beforeEnter: (to: Route, from: Route, next: Next) => {
+      console.log(to, from);
+      if (to.params.hasOwnProperty('row')) {
+        next();
+      } else {
+        next('/');
+      }
+    },
   },
 ];
 
--- src/undeploy.ts
+++ src/undeploy.ts
@@ -1,16 +1,26 @@
 import CONFIG from '@/config';
 
 export async function undeploy(function_name: string): Promise<string> {
+  const url = `${CONFIG.api_url}/undeploy`;
+  const headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json, */*',
+  };
   try {
-    const undeploy_url = `${CONFIG.api_url}/undeploy`;
-    const params = {
-      function_name,
-    };
-    const data = await fetch(undeploy_url, { body: JSON.stringify(params) });
-    const message = await data.text();
-    return message;
+    const request = await fetch(url, {
+      method: 'POST',
+      redirect: 'follow',
+      body: JSON.stringify({function_name}),
+      headers,
+    });
+    const response = await request;
+    if (typeof response.text !== 'function') {
+      return `Encountered an ${response.statusText} trying to deploy ${function_name}}`;
+    }
+    return request.text();
   } catch (error) {
-    console.error(error);
-    throw (error);
+    throw error;
   }
 }
+
+
