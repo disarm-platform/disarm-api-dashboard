6e8f667c85b343a291b981f91e065c58e70ea4c0
manqoba <manqoba.sukati@peoplesized.com>
2020-02-27 15:13:52 +0200

    return status code if error not test_req

--- src/test.ts
+++ src/test.ts
@@ -34,9 +34,12 @@ export async function test(fn_name: string, test_req: any): Promise<string> {
       headers,
     });
     const response = await request;
-    console.log('request:', request);
     if (typeof request.text !== 'function') {
       return `Encountered an ${response.statusText} trying to test ${fn_name}}`;
+    }
+
+    if (response.status !== 200) {
+      return `function returned ${response.status} error.`;
     }
     return request.text();
   } catch (error) {
